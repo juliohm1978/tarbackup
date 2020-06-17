0f27232d20894bf8985da732e9b1201911d70673
Julio H Morimoto <jhm@juliohm.com.br>
2020-06-17 18:45:25 -0300

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,39 @@
+## to restore removing files that no longer exist
+## after the backup was created, use --listed-incremental=/dev/null
+
+DST="$1"
+TYPE="$2"
+
+if [[ "$DST" == "" || "$DST" == "/" ]]; then
+  echo "ERROR Invalid DST=$DST"
+  exit 1
+fi
+
+if [[ "$TYPE" == "full" ]]; then
+  echo "🗑️  Removing all previous archives..."
+  rm -fr $DST/*
+fi
+
+TIMESTAMP_ATUAL=$(date +'%Y%m%d-%H%M%S')
+COUNT=0
+WORKDIR=$(pwd)
+
+mkdir -p "$DST"
+echo "📂 Destination: $DST"
+while IFS= read -r line
+do
+  if [[ "$line" != "" && $line != \#* ]]; then
+    echo "---"
+    BACKUP_HASH=$(echo "$line" | md5sum - | awk '{print $1}')
+    BACKUP_FILE="${BACKUP_HASH}.${TIMESTAMP_ATUAL}.${COUNT}.tar.xz"
+    BACKUP_SNAR="${BACKUP_HASH}.snar"
+    echo "📂 /${line}"
+    echo "🤪 ${BACKUP_SNAR}"
+    echo "📦 ${BACKUP_FILE}"
+    echo "${line}"        >  "${DST}/${BACKUP_HASH}.src"
+    echo "${BACKUP_FILE}" >> "${DST}/${BACKUP_HASH}.history"
+
+    XZ_OPT="-9e -T0" tar -C / --listed-incremental=$DST/${BACKUP_SNAR} --lzma -cf "$DST/${BACKUP_FILE}" "$line"
+    let COUNT=$COUNT+1
+  fi
+done < "${WORKDIR}/filenames"
